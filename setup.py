1ada01f0f8da444767a43fe77b1d7ee23fd82a29
Thing342 <wesjordansemailaddress@gmail.com>
2017-03-26 11:34:00 -0400

    updated install_requires

--- setup.py
+++ setup.py
@@ -32,4 +32,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
+
+    install_requires=['django', 'pytba', 'numpy', 'scipy', 'sklearn'],
 )
